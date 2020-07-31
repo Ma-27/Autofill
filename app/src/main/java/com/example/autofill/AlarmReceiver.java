5b47017d4c45813b4f9632a4e37170341d8fecaf
Ma-27 <mamhsl@163.com>
2020-07-31 16:13:25 +0800

    v3.0 这是第一次提交改进版，主要改进为推送通知代码复用

--- app/src/main/java/com/example/autofill/AlarmReceiver.java
+++ app/src/main/java/com/example/autofill/AlarmReceiver.java
@@ -8,8 +8,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+
+import android.os.Build;
 import android.util.Log;
+
+import java.util.Objects;
 
 import static android.content.ContentValues.TAG;
 import static android.content.Context.MODE_PRIVATE;
@@ -17,10 +23,6 @@ import static com.example.autofill.MainActivity.sharedPrefFile;
 
 public class AlarmReceiver extends BroadcastReceiver
         implements ResponseCode1Interface,ResponseCodeInterface {
-    private NotificationManager mNotificationManager;
-    private static final int NOTIFICATION_ID = 0;
-    private static final String PRIMARY_CHANNEL_ID =
-            "primary_notification_channel";
     public String mData = "";
     public String mSchoolNumber = "";
     public Context mcontext;
@@ -28,11 +30,7 @@ public class AlarmReceiver extends BroadcastReceiver
         @Override
     public void onReceive(Context context, Intent intent) {
             this.mcontext = context;
-        mNotificationManager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Log.d(TAG, "执行一次通知推送成功");
-
-        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPrefFile,
+          SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPrefFile,
                 MODE_PRIVATE);
         mData = sharedPreferences.getString("JSONSeniorString", "");
         Log.d(TAG, "成功恢复数据"+mData);
@@ -59,109 +57,47 @@ public class AlarmReceiver extends BroadcastReceiver
             //网络状况判断
             } else if (mData.length() == 0) {
             //数据库错误
-            deliverNotification2(context);
-            } else {//未知错误
-                deliverNotification2(context);
+            Notify notify3 = new Notify(mcontext,"今日打卡失败","服务器端数据错误");
+            notify3.deliverNotification();
+            } else {
+            //未知原因错误
+            Notify notify2 = new Notify(mcontext,"今日打卡失败","网络连接不良，请重试");
+            notify2.deliverNotification();
             }
         }
 
 
-    //打卡成功
-    private void deliverNotification1(Context context) {
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //该FLAG_UPDATE_CURRENT标志告诉系统使用旧的Intent但替换extras数据。
-        // 因为没有任何额外的功能，所以Intent可以PendingIntent反复使用相同的功能。
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_done_autofill)
-                .setContentTitle("今日已成功打卡！")
-                .setContentText("不需要点击这条通知。如果今天已经手动打卡成功，此消息可以忽略")
-                .setContentIntent(contentPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
-    }
-
-    //未知错误
-    private void deliverNotification2 (Context context) {
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //该FLAG_UPDATE_CURRENT标志告诉系统使用旧的Intent但替换extras数据。
-        // 因为没有任何额外的功能，所以Intent可以PendingIntent反复使用相同的功能。
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_done_autofill)
-                .setContentTitle("今日打卡失败")
-                .setContentText(" 未知原因错误，请手动打卡")
-                .setContentIntent(contentPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
-    }
-
-    //今日已打过卡
-    private void deliverNotification3 (Context context){
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //该FLAG_UPDATE_CURRENT标志告诉系统使用旧的Intent但替换extras数据。
-        // 因为没有任何额外的功能，所以Intent可以PendingIntent反复使用相同的功能。
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_done_autofill)
-                .setContentTitle("今日已打过卡")
-                .setContentText("本APP不再重复打卡")
-                .setContentIntent(contentPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
-    }
-
-    //服务器拒绝了打卡请求
-    private void deliverNotification4 (Context context){
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //该FLAG_UPDATE_CURRENT标志告诉系统使用旧的Intent但替换extras数据。
-        // 因为没有任何额外的功能，所以Intent可以PendingIntent反复使用相同的功能。
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_done_autofill)
-                .setContentTitle("今日打卡失败")
-                .setContentText("打卡请求被服务器拒绝")
-                .setContentIntent(contentPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
-    }
-
     @Override
     public void onCheckPostedFinish(String responsecode1) {
-        Log.d(TAG, "成功returnmessage1"+responsecode1);
         if (responsecode1.equals("TODAYNOFILL")) {
             PostData postData = new PostData();
             postData.delegate = this;
             postData.execute(mData);
         } else if (responsecode1.equals("TODAYHASFILL")) {
-            deliverNotification3(mcontext);
-            Log.d(TAG, "成功执行");
+            Notify notify3 = new Notify(mcontext,"今日已经打过卡","本APP不再重复打卡");
+            notify3.deliverNotification();
         }else{
-            deliverNotification2(mcontext);
+            Notify notify2 = new Notify(mcontext,"今日打卡失败","未知原因错误，请重试");
+            notify2.deliverNotification();
         }
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onPostDataFinish(String responsecode) {
-        if (responsecode == "OK") {
-            deliverNotification1(mcontext);
-        } else if (responsecode == "REFUSE") {
-            deliverNotification4(mcontext);
+        if (Objects.equals(responsecode, "OK")) {
+            //打卡成功
+            Notify notify1 = new Notify(mcontext,"今日打卡成功！","无需点击此通知");
+            notify1.deliverNotification();
+        } else if (Objects.equals(responsecode, "REFUSE")) {
+            //服务器拒绝
+            Notify notify4 = new Notify(mcontext,"今日打卡失败","服务器拒绝了打卡请求");
+            notify4.deliverNotification();
         } else {
-            deliverNotification2(mcontext);
+            //未知原因错误
+            Notify notify2 = new Notify(mcontext,"今日打卡失败","未知原因错误，请重试");
+            notify2.deliverNotification();
         }
     }
 }

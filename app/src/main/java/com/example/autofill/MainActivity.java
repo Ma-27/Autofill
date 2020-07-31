956132073a6d653d0464652a895a88bb32caa93b
Ma-27 <mamhsl@163.com>
2020-07-31 15:13:18 +0800

    Autofill v2.5 initial

--- app/src/main/java/com/example/autofill/MainActivity.java
+++ app/src/main/java/com/example/autofill/MainActivity.java
@@ -42,11 +42,6 @@ public class MainActivity extends AppCompatActivity {
     EditText m_editPhoneNumber;
     EditText m_editCurrentLocation;
     EditText m_editDetailedLocation;
-    EditText m_editRoutine;
-    EditText m_editTransportation;
-    EditText m_editReturnTime;
-    EditText m_editIsolationStartTime;
-    EditText m_editCuttentWhereabouts;
 
     protected JSONObject jsonJuniorParam = new JSONObject();
 
@@ -60,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID =
             "primary_notification_channel";
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,11 +67,6 @@ public class MainActivity extends AppCompatActivity {
         m_editPhoneNumber = findViewById(R.id.editPhoneNumber);
         m_editCurrentLocation = findViewById(R.id.editCurrentLocation);
         m_editDetailedLocation = findViewById(R.id.editDetailedLocation);
-        m_editRoutine = findViewById(R.id.editRoutine);
-        m_editTransportation = findViewById(R.id.editTransportation);
-        m_editReturnTime = findViewById(R.id.editReturnTime);
-        m_editIsolationStartTime = findViewById(R.id.editStartIsolationTime);
-        m_editCuttentWhereabouts = findViewById(R.id.editCurrentWhereabout);
 
 
 
@@ -86,23 +77,12 @@ public class MainActivity extends AppCompatActivity {
         String Name = mPreferences.getString("Name", "");
         String PhoneNumber = mPreferences.getString("PhoneNumber", "");
         String DetailedLocation = mPreferences.getString("DetailedLocation", "");
-        String Routine  = mPreferences.getString("Routine","");
-        String Transportation  = mPreferences.getString("Transportation","");
-        String ReturnTime  = mPreferences.getString("ReturnTime","");
-        String IsolationStartTime  = mPreferences.getString("IsolationStartTime","");
-        String CuttentWhereabouts  = mPreferences.getString("CuttentWhereabouts","");
         m_editSchoolNumber.setText(Schoolnumberstring);
         m_editName.setText(Name);
         m_editAdditionalCondition.setText(AdditionalCondition);
         m_editPhoneNumber.setText(PhoneNumber);
         m_editCurrentLocation.setText(CurrentLocation);
         m_editDetailedLocation.setText(DetailedLocation);
-        m_editRoutine.setText(Routine);
-        m_editTransportation.setText(Transportation);
-        m_editReturnTime.setText(ReturnTime);
-        m_editIsolationStartTime.setText(IsolationStartTime);
-        m_editCuttentWhereabouts.setText(CuttentWhereabouts);
-
 
         //开启后台推送
         if(!isIgnoringBatteryOptimizations());{
@@ -315,32 +295,6 @@ public class MainActivity extends AppCompatActivity {
             }else {
                 jsonJuniorParam.put("beizhu",m_editAdditionalCondition.getText().toString());
             }
-            if(TextUtils.isEmpty(m_editRoutine.getText())){
-                jsonJuniorParam.put("xb","");
-            }else {
-                jsonJuniorParam.put("xb",m_editRoutine.getText().toString());
-            }
-            if(TextUtils.isEmpty(m_editTransportation.getText())){
-                jsonJuniorParam.put("fyjtgj", "无");//返渝交通工具
-            }else {
-                jsonJuniorParam.put("fyjtgj",m_editTransportation.getText().toString());
-            }
-            if(TextUtils.isEmpty(m_editReturnTime.getText())){
-                jsonJuniorParam.put("fyddsj", "无");//返渝时间
-            }else {
-                jsonJuniorParam.put("fyddsj",m_editReturnTime.getText().toString());
-            }
-            if(TextUtils.isEmpty(m_editIsolationStartTime.getText())){
-                jsonJuniorParam.put("jjglqssj", "无");//居家隔离起始时间
-            }else {
-                jsonJuniorParam.put("jjglqssj",m_editIsolationStartTime.getText().toString());
-            }
-            if(TextUtils.isEmpty(m_editCuttentWhereabouts.getText())){
-                jsonJuniorParam.put("wjjglmqqx", "无");//为居家隔离目前去向
-            }else {
-                jsonJuniorParam.put("wjjglmqqx",m_editCuttentWhereabouts.getText().toString());
-            }
-
             jsonJuniorParam.put("sign","c4cb4738a0b923820scc509a6f75849b");
             jsonJuniorParam.put("xh",m_editSchoolNumber.getText().toString());
             jsonJuniorParam.put("name",m_editName.getText().toString());
@@ -353,40 +307,6 @@ public class MainActivity extends AppCompatActivity {
         return jsonJuniorParam.toString();
     }
 
-    public void onJudge2ButtonClicked(View view) {
-        try {
-            boolean checked = ((RadioButton) view).isChecked();
-            switch (view.getId()) {
-                case R.id.newyes1:
-                    if (checked) {
-                        jsonJuniorParam.put("sfbgsq", "有");//是报告社区
-                    }//接触hb
-                    break;
-                case R.id.newno1:
-                    if (checked) {
-                        jsonJuniorParam.put("sfbgsq", "无");//否报告社区
-                    }//接触hb
-                    break;
-                case R.id.newyes2:
-                    if (checked) {
-                        jsonJuniorParam.put("sfjjgl", "有");//是居家隔离
-                    }//接触hb
-                    break;
-                case R.id.newno2:
-                    if (checked) {
-                        jsonJuniorParam.put("sfjjgl", "无");//不是居家隔离
-                    }//接触hb
-                    break;
-                default:{
-                    jsonJuniorParam.put("sfbgsq", "无");//否报告社区
-                    jsonJuniorParam.put("sfjjgl", "无");//不是居家隔离
-                }
-                    break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     private void EncodeJsonJuniorParamNo(){
         try {
@@ -415,12 +335,6 @@ public class MainActivity extends AppCompatActivity {
         preferencesEditor.putString("PhoneNumber",m_editPhoneNumber.getText().toString());
         preferencesEditor.putString("DetailedLocation",m_editDetailedLocation.getText().toString());
         preferencesEditor.putString("JSONSeniorString",EncodeJSONSeniorParam());
-        preferencesEditor.putString("Routine",m_editRoutine.getText().toString());
-        preferencesEditor.putString("Transportation",m_editTransportation.getText().toString());
-        preferencesEditor.putString("ReturnTime",m_editReturnTime.getText().toString());
-        preferencesEditor.putString("Transportation",m_editTransportation.getText().toString());
-        preferencesEditor.putString("IsolationStartTime",m_editIsolationStartTime.getText().toString());
-        preferencesEditor.putString("CuttentWhereabouts",m_editCuttentWhereabouts.getText().toString());
 
         preferencesEditor.apply();
     }

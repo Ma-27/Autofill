6209dd38bd0f8013c1e412c2ac426d956966e617
Ma-27 <mamhsl@163.com>
2020-08-28 17:44:32 +0800

    设置fcm，初始化设置界面

--- app/src/main/java/com/example/autofill/setting/SettingsActivity.java
+++ app/src/main/java/com/example/autofill/setting/SettingsActivity.java
@@ -1,33 +1,41 @@
 package com.example.autofill.setting;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.example.autofill.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    public static final String
+            KEY_PREF_FCM_SWITCH = "use_fcm_switch";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        //setContentView(R.layout.settings_activity);
+        //如果类扩展Activity,那么Fragment延伸PreferenceFragment
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
+                .replace(android.R.id.content, new SettingsFragment())
                 .commit();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        PreferenceManager.setDefaultValues(this,R.xml.root_preferences,false);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean switchPref = sharedPref.getBoolean
+                (SettingsActivity.KEY_PREF_FCM_SWITCH, false);
+       //处理默认值
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
+
 }
---
+++ app/src/main/java/com/example/autofill/setting/SettingsFragment.java
@@ -0,0 +1,14 @@
+package com.example.autofill.setting;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+import com.example.autofill.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences,rootKey);
+
+    }
+}
